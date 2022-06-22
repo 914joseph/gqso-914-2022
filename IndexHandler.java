dc42e120b85631fda8ec5849410465216f4de109
thayingrid <tip1@aluno.ifal.edu.br>
2022-06-22 15:05:57 +0000

    todo

--- IndexHandler.java
+++ IndexHandler.java
@@ -1,8 +1,5 @@
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
-import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;   
 
