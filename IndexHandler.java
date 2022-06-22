7c92895ab3d105df8beb0afb2efd5481445b59ad
evic08 <evcc1@aluno.ifal.edu.br>
2022-06-22 14:25:29 +0000

    adicionando operacao da divisao

--- IndexHandler.java
+++ IndexHandler.java
@@ -8,7 +8,7 @@ public class IndexHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         try {
-            byte[] resposta = "<h1>Java nao eh chatinho</h1>".getBytes();
+            byte[] resposta = "<h1> Calculadora</h1>".getBytes();
             OutputStream corpoResposta = exchange.getResponseBody();
             
             exchange.sendResponseHeaders(200, resposta.length);
