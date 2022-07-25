7211211b3d856bc10973509d5e40a302dbb1144f
914joseph <jfsj5@aluno.ifal.edu.br>
2022-07-25 09:01:55 -0300

    mandando testesoma

---
+++ api/src/test/java/com/danielfireman/ifal/calcapi/SomaTest.java
@@ -0,0 +1,31 @@
+package com.danielfireman.ifal.calcapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import io.jooby.MockRouter;
+import io.jooby.StatusCode;
+import io.jooby.exception.BadRequestException;
+
+public class SomaTest {
+
+  @Test
+  public void soma() {
+    MockRouter router = new MockRouter(new App());
+    router.get("/soma/9/1", rsp -> {
+      assertEquals(10.0, rsp.value());
+      assertEquals(StatusCode.OK, rsp.getStatusCode());
+    });
+  }
+
+  @Test
+  public void soma_opString() {
+    MockRouter router = new MockRouter(new App());
+    assertThrows(BadRequestException.class,
+    ()->{
+      router.get("/soma/aa", rsp -> {});
+    });
+  }
+}
